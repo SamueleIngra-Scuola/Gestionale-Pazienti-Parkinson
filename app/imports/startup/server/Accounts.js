ea190c0b9f707c73dda785a7e99292fc598cd183
S4pphire <ingrassiasamu@gmail.com>
2023-05-12 12:36:32 +0200

    refactor: Change name

--- app/imports/startup/server/Accounts.js
+++ app/imports/startup/server/Accounts.js
@@ -4,6 +4,19 @@ import { Roles } from 'meteor/alanning:roles';
 
 /* eslint-disable no-console */
 
+/* const createUser = (email, password, role) => {
+  console.log(`  Creating user ${email}.`);
+  const userID = Accounts.createUser({
+    username: email,
+    email: email,
+    password: password,
+  });
+  if (role === 'admin') {
+    Roles.createRole(role, { unlessExists: true });
+    Roles.addUsersToRoles(userID, 'admin');
+  }
+}; */
+
 const createUser = (email, password, role) => {
   console.log(`  Creating user ${email}.`);
   const userID = Accounts.createUser({
