10719be4c098e2a6de85ae9f6fcc83c117b983a9
MDL <marco.dilena@studenti.ittsrimini.edu.it>
2023-05-15 03:24:55 +0200

    feat: Change SigUp

--- app/imports/ui/pages/SignUp.jsx
+++ app/imports/ui/pages/SignUp.jsx
@@ -8,7 +8,7 @@ import SimpleSchema from 'simpl-schema';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import { AutoForm, ErrorsField, SubmitField, TextField } from 'uniforms-bootstrap5';
 import { FileOutlined, PieChartOutlined, UserOutlined } from '@ant-design/icons';
-import { Breadcrumb, Layout, Menu, theme, Select, Space } from 'antd';
+import { Breadcrumb, Layout, Menu, theme, Select, Space, Button, Form, Input, DatePicker,} from 'antd';
 // import { dayjs } from 'dayjs';
 
 // eslint-disable-next-line no-var
@@ -33,11 +33,13 @@ const SignUp = ({ location }) => {
     birthplace: String,
     role: String,
   });
+  
   const bridge = new SimpleSchema2Bridge(schema);
 
   /* Handle SignUp submission. Create user account and a profile entry, then redirect to the home page. */
   const submit = (doc) => {
     // const { email, password } = doc;
+    const { email, password, name, surname, phone, birthday, birthplace, role } = doc;
     // let { birthday } = doc;
     /* Accounts.createUser({ email, username: email, password }, (err) => {
       if (err) {
@@ -49,7 +51,7 @@ const SignUp = ({ location }) => {
     }); */
     // birthday = dayjs.utc(birthday).format('DD/MM/YYYY');
     // console.log(birthday);
-    Meteor.call('createUserAccount', doc, (err, result) => {
+    Meteor.call('createUserAccount', email, password, name, surname, phone, birthday, birthplace, role, (err, result) => {
       if (err) {
         setError(err.reason);
       } else {
@@ -67,62 +69,210 @@ const SignUp = ({ location }) => {
   if (redirectToReferer) {
     return <Navigate to={from} />;
   }
+}
+  const { Option } = Select;
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24
+    },
+    sm: {
+      span: 8
+    }
+  },
+  wrapperCol: {
+    xs: {
+      span: 24
+    },
+    sm: {
+      span: 16
+    }
+  }
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0
+    },
+    sm: {
+      span: 16,
+      offset: 8
+    }
+  }
+};
+  const SignUp = () => {
+    const [form] = Form.useForm();
+    const prefixSelector = (
+      <Form.Item name="prefix" noStyle>
+        <Select
+          style={{
+            width: 70
+          }}
+        >
+          <Option value="39">+39</Option>
+          <Option value="378">+378</Option>
+        </Select>
+      </Form.Item>
+    );
   return (
-    <Container id="signup-page" className="py-3">
-      <Row className="justify-content-center">
-        <Col xs={5}>
-          <Col className="text-center">
-            <h2>Register your account</h2>
-          </Col>
-          <AutoForm schema={bridge} onSubmit={data => submit(data)}>
-            <Card>
-              <Card.Body>
-                <TextField name="email" placeholder="Indirizzo E-Mail" />
-                <TextField name="password" placeholder="Password" type="password" />
-                <TextField name="name" placeholder="Nome" />
-                <TextField name="surname" placeholder="Cognome" />
-                <TextField name="phone" placeholder="+39 000 000 0000" />
-                <TextField name="birthday" placeholder="DD/MM/YYYY" />
-                <TextField name="birthplace" placeholder="Comune di Nascita" />
-                <Space wrap style={{marginBottom: 10}}>
-                  <Select
-                    defaultValue="patient"
-                    style={{
-                      width: 120
-                    }}
-                    options={[
-                      {
-                        value: "medic",
-                        label: "Medico"
-                      },
-                      {
-                        value: "patient",
-                        label: "Paziente"
-                      },
-                    ]}
-                  />
-                </Space>
-                <ErrorsField />
-                <SubmitField />
-              </Card.Body>
-            </Card>
-          </AutoForm>
-          <Alert variant="light">
-            Already have an account? Login
-            {' '}
-            <Link to="/signin">here</Link>
-          </Alert>
-          {error === '' ? (
-            ''
-          ) : (
-            <Alert variant="danger">
-              <Alert.Heading>Errore: </Alert.Heading>
-              {error}
-            </Alert>
-          )}
-        </Col>
-      </Row>
-    </Container>
+    <Form
+      {...formItemLayout}
+      form={form}
+      name="register"
+      initialValues={{
+        prefix: "39"
+      }}
+      style={{
+        maxWidth: 600
+      }}
+      scrollToFirstError
+    >
+      <Form.Item
+        name="name"
+        label="Nome"
+        rules={[
+          {
+            type: "name",
+            message: "Inserire un nome valido!"
+          },
+          {
+            required: true,
+            message: "Inserire un nome valido!"
+          }
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="surname"
+        label="Cognome"
+        rules={[
+          {
+            type: "surname",
+            message: "Inserire un cognome valido!"
+          },
+          {
+            required: true,
+            message: "Inserire un cognome valido!"
+          }
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+      name="date"
+      label="Data di Nascita"
+      rules={[
+        {
+          type: "date",
+          message: "The input is not valid date"
+        },
+        {
+          required: true,
+          message: "Per favore inserisci la tua data di nascita"
+        }
+      ]}
+      >
+          <DatePicker />
+        </Form.Item>
+      <Form.Item
+        name="email"
+        label="E-mail"
+        rules={[
+          {
+            type: "email",
+            message: "The input is not valid E-mail!"
+          },
+          {
+            required: true,
+            message: "Please input your E-mail!"
+          }
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="password"
+        label="Password"
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!"
+          }
+        ]}
+        hasFeedback
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="confirm"
+        label="Confirm Password"
+        dependencies={["password"]}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: "Please confirm your password!"
+          },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue("password") === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject(
+                new Error("The two passwords that you entered do not match!")
+              );
+            }
+          })
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item
+        name="phone"
+        label="Phone Number"
+        rules={[
+          {
+            required: true,
+            message: "Please input your phone number!"
+          }
+        ]}
+      >
+        <Input
+          addonBefore={prefixSelector}
+          style={{
+            width: "100%"
+          }}
+        />
+      </Form.Item>
+
+      <Form.Item
+        name="role"
+        label="Tipo Account:"
+        rules={[
+          {
+            required: true,
+            message: "Inserire il tipo di account!"
+          }
+        ]}
+      >
+        <Select defaultValue="patient">
+          <Option value="patient">Paziente</Option>
+          <Option value="medic">Medico</Option>
+        </Select>
+      </Form.Item>
+      <Form.Item {...tailFormItemLayout}>
+        <Button type="primary" htmlType="submit">
+          Registrati
+        </Button>
+        <Button type="link" htmlType="submit" style={{marginLeft:5}}>
+          Hai già un account? Accedi qui
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
 
